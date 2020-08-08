bc8590f6bb624aa2fdd76f6ff8d9856cf3ece203
Ali Deishidi <sizief@gmail.com>
2020-08-08 18:10:23 +0430

    add item page

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 ## TODO
 [x] First page on prod is accessible under dummy URL
 [x] CI
-[] First page
-[] Collection
+[x] First page
+[x] Collection
 [] Item
+[x] Page titles
 [] Domain
 [] Contracts on testnet
 [] Contracts on Mainnet
 [] Party!
+
--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -3,6 +3,11 @@ class ItemController < ApplicationController
 
   def index
     @item = data.dig(params[:collection], :items, params[:item])
+    @collection = data[params[:collection]]
+
+    owner = Struct.new(:address, :buy_at)
+    @owners = [["0x123456781233", Date.today],["0x3423456225", Date.today]]
+      .map { |o| owner.new(o.first, o.last) }
   end
 
   private
